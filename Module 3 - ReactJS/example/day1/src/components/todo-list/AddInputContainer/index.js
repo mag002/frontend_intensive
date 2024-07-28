eb355f3ecd94a82f79d7b7b48bb0f5afc445fde0
Le phuc <lephuc@192.168.2.5>
2024-07-28 18:42:37 +0700

    Day 39 - TodoList

--- Module 3 - ReactJS/example/day1/src/components/todo-list/AddInputContainer/index.js
+++ Module 3 - ReactJS/example/day1/src/components/todo-list/AddInputContainer/index.js
@@ -1,10 +1,26 @@
 import { Button, TextField, Box } from '@mui/material';
+import { useState } from 'react';
 
-const AddInputContainer = () => {
-    return <Box p={4} display="flex" gap={3}>
-
-        <TextField id="outlined-basic" label="Add new Item" variant="outlined" />
-        <Button>Add</Button>
+const AddInputContainer = ({ handleAddTodo }) => {
+    const [inputValue, setInputValue] = useState('')
+    const [error, setError] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError('')
+        if (!inputValue) {
+            setError('Invalid task name')
+            return
+        }
+        handleAddTodo(inputValue);
+        setInputValue('')
+    }
+    //  Complete add function
+    // - Step 1: change from button click event to form submit [17:10]
+    // - Step 2 (Homework): focus back to input when submit (keyword: useRef)
+    // falsy value= "" ~ false "--" ~ true
+    return <Box component="form" p={4} display="flex" gap={3} onSubmit={handleSubmit}>
+        <TextField error={!!error} helperText={error} id="outlined-basic" label="Add new Item" variant="outlined" value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
+        <Button type="submit" >Add</Button>
     </Box>
 }
 
--- Module 3 - ReactJS/example/day1/src/components/todo-list/TodoItem/index.js
+++ Module 3 - ReactJS/example/day1/src/components/todo-list/TodoItem/index.js
@@ -1,6 +1,7 @@
 import { Button, Box, Checkbox, Typography } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import styled from 'styled-components'
+import StyledGridBox from '../StyledGridBox';
 
 const StyledParagraph = styled(Typography)`
     margin-bottom: 0px!important;
@@ -8,19 +9,21 @@ const StyledParagraph = styled(Typography)`
     align-items: center;
 `
 
-const TodoItem = () => {
-    return <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Box display="flex" gap={1}>
-            <Checkbox inputProps={{ 'aria-label': 'Checkbox demo' }} defaultChecked />
-            <StyledParagraph variant="body1" gutterBottom>
-                Task name
-            </StyledParagraph>
-        </Box>
+const TodoItem = ({ id, name, isDone, createdAt, handleUpdateTodo, handleDeleteTodo }) => {
+    return <StyledGridBox id={id} >
+        <Checkbox onChange={() => handleUpdateTodo(id)} inputProps={{ 'aria-label': 'Checkbox demo' }} checked={isDone} />
+        <StyledParagraph variant="body1" >
+            {name}
+        </StyledParagraph>
+        <StyledParagraph variant="body1" >
+            {createdAt}
+        </StyledParagraph>
         <Box>
-            <Button>
+            <Button onClick={() => handleDeleteTodo(id)}>
                 <CloseIcon />
             </Button>
         </Box>
-    </Box>
+    </StyledGridBox>
+
 }
 export default TodoItem
